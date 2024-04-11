d7648da01af3f7e975d02db24dc49a1d830d3a0f
Valerio Setti <valerio.setti@nordicsemi.no>
2024-04-11 11:50:46 +0200

    fix "make generated_files" for test_keys.h and test_certs.h
    This also add the check in tests/scripts/check-generated-files.sh
    
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- tests/scripts/generate_test_keys.py
+++ tests/scripts/generate_test_keys.py
@@ -77,9 +77,9 @@ def main():
     args = argparser.parse_args()
 
     output_file = args.output
-    # Remove output file if already existing.
+    # If the output file already exists, then we can quit (successfully)
     if os.path.exists(output_file):
-        os.remove(output_file)
+        return
 
     output_file = open(output_file, 'at')
     output_file.write(
